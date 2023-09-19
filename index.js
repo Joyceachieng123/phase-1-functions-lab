7e9a181efb062c913a407f35e1fd7ec748d12127
Joyce Achieng <joyce.achieng@student.moringaschool.com>
2023-09-19 17:17:06 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+  }
+  
+  function distanceFromHqInFeet(pickupLocation) {
+    const block = distanceFromHqInBlocks(pickupLocation);
+    const feet = block * 264;
+    return feet
+  }
+  
+  function distanceTravelledInFeet(start, destination) {
+    const blocks = Math.abs(start - destination);
+    const feet = blocks * 264;
+    return feet
+  }
+  function calculatesFarePrice(start, destination) {
+    const feet = distanceTravelledInFeet(start, destination);
+    
+    if (feet <= 400) {
+      return 0;
+    } else if (feet > 400 && feet <= 2000) {
+      return (feet - 400) * 0.02;
+    } else if (feet > 2000 && feet <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far';
+    }
+  }
+  
+  
+
+  
